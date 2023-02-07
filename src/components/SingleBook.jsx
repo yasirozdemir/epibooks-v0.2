061d987002333da1abdc22f852f4e72c10c65a60
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-07 18:40:46 +0300

    single book component uses hooks now

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,46 +1,38 @@
-import { Component } from "react";
 import { Card, Col } from "react-bootstrap";
+import { useState } from "react";
 
-class SingleBook extends Component {
-  state = {
-    selected: false,
-  };
+const SingleBook = (props) => {
+  const [selected, setSelected] = useState(false);
 
-  render() {
-    return (
-      <Col xs={10} sm={6} md={4} lg={3} className="mb-3">
-        <Card
-          onClick={() => {
-            this.props.setBookID(this.props.book.asin);
-            if (this.state.selected) {
-              this.setState({
-                selected: false,
-              });
-              this.props.setBookID(undefined);
-            } else {
-              this.setState({
-                selected: true,
-              });
-            }
-          }}
-          className={this.state.selected ? "selected bookCard" : "bookCard"}
-        >
-          <Card.Img variant="top" src={this.props.book.img} />
-          <Card.Body>
-            <Card.Title>{this.props.book.title}</Card.Title>
-            <Card.Text>
-              <span className="text-primary">${this.props.book.price}</span>
-              <span className="d-none d-md-inline">
-                {" | "}
-                {this.props.book.category.charAt(0).toUpperCase() +
-                  this.props.book.category.slice(1)}
-              </span>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    );
-  }
-}
+  return (
+    <Col xs={10} sm={6} md={4} lg={3} className="mb-3">
+      <Card
+        onClick={() => {
+          props.setBookID(props.book.asin);
+          if (selected) {
+            setSelected(false);
+            props.setBookID(undefined);
+          } else {
+            setSelected(true);
+          }
+        }}
+        className={selected ? "selected bookCard" : "bookCard"}
+      >
+        <Card.Img variant="top" src={props.book.img} />
+        <Card.Body>
+          <Card.Title>{props.book.title}</Card.Title>
+          <Card.Text>
+            <span className="text-primary">${props.book.price}</span>
+            <span className="d-none d-md-inline">
+              {" | "}
+              {props.book.category.charAt(0).toUpperCase() +
+                props.book.category.slice(1)}
+            </span>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
 export default SingleBook;
