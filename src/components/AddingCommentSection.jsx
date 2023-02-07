565598d07920f318390ff55951cc4d010789deb6
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-02-07 18:59:33 +0300

    AddingCommentSection.jsx now works using hooks

--- src/components/AddingCommentSection.jsx
+++ src/components/AddingCommentSection.jsx
@@ -1,23 +1,21 @@
-import { Component } from "react";
+import { useState } from "react";
 import { FormGroup, Form, Button, Alert } from "react-bootstrap";
+const url = "https://striveschool-api.herokuapp.com/api/comments/";
 
-class AddingCommentSection extends Component {
-  state = {
-    url: "https://striveschool-api.herokuapp.com/api/comments/",
-    newComment: {
-      comment: "",
-      rate: 0,
-      elementId: this.props.bookID,
-    },
-    isError: false,
-    isUploaded: false,
-  };
+const AddingCommentSection = (props) => {
+  const [newComment, setNewComment] = useState({
+    comment: "",
+    rate: 0,
+    elementId: props.bookID,
+  });
+  const [isError, setIsError] = useState(false);
+  const [isUploaded, setIsUploaded] = useState(false);
 
-  postComment = async () => {
+  const postComment = async () => {
     try {
-      const response = await fetch(this.state.url, {
+      const response = await fetch(url, {
         method: "POST",
-        body: JSON.stringify(this.state.newComment),
+        body: JSON.stringify(newComment),
         headers: {
           Authorization:
             "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M5MzgxYWU3MzczODAwMTUzNzQzN2MiLCJpYXQiOjE2NzUzNDI4NzAsImV4cCI6MTY3NjU1MjQ3MH0.XvSJCouk9YeDJk4keaXoIOlB-nkCaRWTmZAqhXXgSGQ",
@@ -25,91 +23,72 @@ class AddingCommentSection extends Component {
         },
       });
       if (response.ok) {
-        this.setState({
-          ...this.state,
-          isUploaded: true,
-          newComment: {
-            ...this.state.newComment,
-            comment: "",
-            rate: 0,
-          },
+        setIsUploaded(true);
+        setNewComment({
+          ...newComment,
+          comment: "",
+          rate: 0,
         });
       } else {
-        this.setState({
-          ...this.state,
-          isError: true,
-        });
+        setIsError(true);
       }
     } catch (error) {
-      this.setState({
-        ...this.state,
-        isError: true,
-      });
+      setIsError(true);
     }
   };
 
-  render() {
-    return (
-      <>
-        <h6 className="mt-2">Add comment</h6>
-        {this.state.isError && (
-          <Alert variant="danger">
-            The comment couldn't saved. Please try again!
-          </Alert>
-        )}
-        {this.state.isUploaded && (
-          <Alert variant="success">Thanks for the comment!</Alert>
-        )}
-        <Form
-          onSubmit={(event) => {
-            event.preventDefault();
-            this.postComment();
-          }}
-        >
-          <FormGroup>
-            <Form.Control
-              as="textarea"
-              placeholder="Your comment..."
-              value={this.state.newComment.comment}
-              onChange={(event) => {
-                this.setState({
-                  ...this.state,
-                  newComment: {
-                    ...this.state.newComment,
-                    comment: event.target.value,
-                  },
-                });
-              }}
-            />
-          </FormGroup>
-          <FormGroup className="d-flex align-items-center">
-            <label htmlFor="rateInput">Rate:</label>
-            <input
-              className="ml-2 w-50"
-              type="number"
-              id="rateInput"
-              required
-              min={0}
-              max={5}
-              value={this.state.newComment.rate}
-              onChange={(event) => {
-                this.setState({
-                  ...this.state,
-                  newComment: {
-                    ...this.state.newComment,
-                    rate: event.target.value,
-                  },
-                });
-              }}
-            />
-            <Button className="ml-auto" type="submit" variant="dark">
-              Send
-            </Button>
-          </FormGroup>
-        </Form>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h6 className="mt-2">Add comment</h6>
+      {isError && (
+        <Alert variant="danger">
+          The comment couldn't saved. Please try again!
+        </Alert>
+      )}
+      {isUploaded && <Alert variant="success">Thanks for the comment!</Alert>}
+      <Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          postComment();
+        }}
+      >
+        <FormGroup>
+          <Form.Control
+            as="textarea"
+            placeholder="Your comment..."
+            value={newComment.comment}
+            onChange={(event) => {
+              setNewComment({
+                ...newComment,
+                comment: event.target.value,
+              });
+            }}
+          />
+        </FormGroup>
+        <FormGroup className="d-flex align-items-center">
+          <label htmlFor="rateInput">Rate:</label>
+          <input
+            className="ml-2 w-50"
+            type="number"
+            id="rateInput"
+            required
+            min={0}
+            max={5}
+            value={newComment.rate}
+            onChange={(event) => {
+              setNewComment({
+                ...newComment,
+                rate: event.target.value,
+              });
+            }}
+          />
+          <Button className="ml-auto" type="submit" variant="dark">
+            Send
+          </Button>
+        </FormGroup>
+      </Form>
+    </>
+  );
+};
 
 export default AddingCommentSection;
